85ad65d7f6d260d71fc82eac6c8339890fd95084
Robert Csakany <robert.csakany@blackbelt.hu>
2018-09-21 17:36:05 +0200

    JUDO-3182 Add access interface for Factory

---
+++ src/main/java/hu/blackbelt/judo/meta/rdbms/RdbmsMetaModel.java
@@ -0,0 +1,8 @@
+package hu.blackbelt.judo.meta.rdbms;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+public interface RdbmsMetaModel {
+
+    Resource.Factory getFactory();
+}
--- src/main/java/hu/blackbelt/judo/meta/rdbms/RdbmsMetaModelRegistration.java
+++ src/main/java/hu/blackbelt/judo/meta/rdbms/RdbmsMetaModelRegistration.java
@@ -11,23 +11,31 @@ import org.osgi.service.component.annotations.Deactivate;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-@Component(immediate = true)
-public class RdbmsMetaModelRegistration {
+@Component(immediate = true, service = RdbmsMetaModel.class)
+public class RdbmsMetaModelRegistration implements RdbmsMetaModel {
 
     ServiceRegistration<Resource.Factory> rdbmsFactoryRegistration;
+    Resource.Factory factory;
 
     @Activate
     public void activate(ComponentContext componentContext) {
         Dictionary<String, Object> params = new Hashtable<>();
         params.put("meta", "rdbms");
         params.put("version", componentContext.getBundleContext().getBundle().getVersion());
+        params.put("bundle", componentContext.getBundleContext().getBundle());
 
+        factory = new RdbmsResourceFactoryImpl();
         rdbmsFactoryRegistration = componentContext.getBundleContext()
-                .registerService(Resource.Factory.class, new RdbmsResourceFactoryImpl(), params);
+                .registerService(Resource.Factory.class, factory, params);
     }
 
     @Deactivate
     public void deactivate() {
         rdbmsFactoryRegistration.unregister();
+    }
+
+    @Override
+    public Resource.Factory getFactory() {
+        return factory;
     }
 }
