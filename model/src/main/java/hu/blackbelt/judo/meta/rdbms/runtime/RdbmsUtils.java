7384010d4fdbf38167a63698df9f768b93881193
Gelei Bence <bence.gelei@blackbelt.hu>
2020-04-02 14:33:30 +0200

    JNG-1259 Utils updated

--- model/src/main/java/hu/blackbelt/judo/meta/rdbms/runtime/RdbmsUtils.java
+++ model/src/main/java/hu/blackbelt/judo/meta/rdbms/runtime/RdbmsUtils.java
@@ -1,19 +1,16 @@
 package hu.blackbelt.judo.meta.rdbms.runtime;
-import org.eclipse.emf.common.notify.Notifier;
-import org.eclipse.emf.common.util.ECollections;
-import org.eclipse.emf.common.util.EMap;
-import org.eclipse.emf.common.util.UniqueEList;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import hu.blackbelt.judo.meta.rdbms.RdbmsField;
+import hu.blackbelt.judo.meta.rdbms.RdbmsTable;
+import hu.blackbelt.judo.meta.rdbms.support.RdbmsModelResourceSupport;
+import lombok.Getter;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
 
 public class RdbmsUtils {
 
@@ -23,9 +20,26 @@ public class RdbmsUtils {
 
     private ResourceSet resourceSet;
 
-    public RdbmsUtils() {
+    @Getter
+    private RdbmsModelResourceSupport rdbmsModelResourceSupport;
+    @Getter
+    private RdbmsModel rdbmsModel;
+
+    //////////////////////////////////////////////////
+    ////////////////// CONSTRUCTORS //////////////////
+
+    /**
+     * Create RdbmsUtils by passing the RdbmsModel itself
+     * @param rdbmsModel Rdbms model to "build" utils on
+     */
+    public RdbmsUtils(RdbmsModel rdbmsModel) {
+        this.rdbmsModel = rdbmsModel;
+        rdbmsModelResourceSupport = RdbmsModelResourceSupport.rdbmsModelResourceSupportBuilder()
+                .resourceSet(rdbmsModel.getResourceSet())
+                .uri(rdbmsModel.getUri())
+                .build();
     }
-    
+
     public RdbmsUtils(final ResourceSet resourceSet) {
         this(resourceSet, false);
     }
@@ -37,7 +51,73 @@ public class RdbmsUtils {
         // TODO: Processes here
     }
 
+    //////////////////////////////////////////////////
+    ///////////////// OTHER METHODS //////////////////
+
     public void setFailOnError(final boolean failOnError) {
         this.failOnError = failOnError;
     }
+
+    //////////////////////////////////////////////////
+    //////////////////// TABLES //////////////////////
+
+    /**
+     * Get all RdbmsTable from RdbmsModel
+     * @return all RdbmsTable if exists
+     */
+    public Optional<EList<RdbmsTable>> getRdbmsTables() {
+        //TODO: Tests
+        BasicEList<RdbmsTable> rdbmsTables = new BasicEList<>();
+        rdbmsModelResourceSupport.getStreamOfRdbmsRdbmsTable().forEach(rdbmsTables::add);
+        return !(rdbmsTables.isEmpty())
+                ? Optional.of(rdbmsTables)
+                : Optional.empty();
+    }
+
+    /**
+     * Get certain RdbmsTable
+     * @param rdbmsTableName RdbmsTable's name to search for (packagename.classname)
+     * @return RdbmsTable if exists
+     */
+    public Optional<RdbmsTable> getRdbmsTable(String rdbmsTableName) {
+        //TODO: Tests
+        return getRdbmsTables().isPresent()
+                ? getRdbmsTables().get().stream().filter(o -> rdbmsTableName.equals(o.getName())).findAny()
+                : Optional.empty();
+    }
+
+    //////////////////////////////////////////////////
+    //////////////////// FIELDS //////////////////////
+
+    /**
+     * Get all RdbmsField from certain RdbmsTable
+     * @param rdbmsTableName RdbmsTable's name to get all RdbmsField from (packagename.classname)
+     * @return All RdbmsField if exists
+     */
+    public Optional<EList<RdbmsField>> getRdbmsFields(String rdbmsTableName) {
+        //TODO: Tests
+        return getRdbmsTable(rdbmsTableName).isPresent() && !(getRdbmsTable(rdbmsTableName).get().getFields().isEmpty())
+                ? Optional.of(getRdbmsTable(rdbmsTableName).get().getFields())
+                : Optional.empty();
+    }
+
+    /**
+     * Get certain RdbmsField from given RdbmsTable
+     * @param rdbmsTableName RdbmsTable's name to search in (packagename.classname)
+     * @param rdbmsFieldName RdbmsField's name to search for
+     * @param concatNames true if during search, rdbmsTableName and rdbmsFieldName will be concatenated with '#' between them
+     * @return RdbmsField if exists
+     */
+    @SuppressWarnings("OptionalGetWithoutIsPresent")
+    public Optional<RdbmsField> getRdbmsField(String rdbmsTableName, String rdbmsFieldName, boolean concatNames) {
+        //TODO: Tests
+        final String finalRdbmsFieldName =
+                concatNames
+                        ? rdbmsTableName + "#" + rdbmsFieldName
+                        : "#" + rdbmsFieldName;
+        return (getRdbmsFields(rdbmsTableName).isPresent() && getRdbmsFields(rdbmsTableName).get().stream().anyMatch(o -> finalRdbmsFieldName.equals(o.getName())))
+                ? Optional.of(getRdbmsFields(rdbmsTableName).get().stream().filter(o -> finalRdbmsFieldName.equals(o.getName())).findAny().get())
+                : Optional.empty();
+    }
+
 }
