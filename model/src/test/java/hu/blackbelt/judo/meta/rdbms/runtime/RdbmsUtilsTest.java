a4479a557714224e0e1bed90013361969ce00b6f
Gelei Bence <bence.gelei@blackbelt.hu>
2020-04-06 15:13:15 +0200

    JNG-1259 Test file added

---
+++ model/src/test/java/hu/blackbelt/judo/meta/rdbms/runtime/RdbmsUtilsTest.java
@@ -0,0 +1,10 @@
+package hu.blackbelt.judo.meta.rdbms.runtime;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class RdbmsUtilsTest {
+    @BeforeEach
+    public void setUp() {
+        //TODO
+    }
+}
