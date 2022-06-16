df6b545325ffab6907eb498963cd5f1b2791c9ab
Robert Csakany <robert.csakany@blackbelt.hu>
2022-06-16 15:47:02 +0200

    JNG-3784 Update dependencies

--- update-target-versions.sh
+++ update-target-versions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn validate -Dtycho.mode=maven -P update-target-versions -f ./targetdefinition/pom.xml
-mvn validate -Dtycho.mode=maven -P update-category-versions -f ./site/pom.xml
+mvn install -Dtycho.mode=maven -P update-target-versions -f ./targetdefinition/pom.xml
+mvn validate -DskipModules -Dtycho.mode=maven -P update-category-versions
