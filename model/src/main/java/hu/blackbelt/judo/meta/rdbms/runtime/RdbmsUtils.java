3a6bd1d6620106d54f72e0ab9a26d2dc15694d13
gaborflorian <114913193+gaborflorian@users.noreply.github.com>
2023-03-24 15:16:57 +0100

    JNG-4625 Judo ng project replacment (#56)
    * JNG-4625 Initial feature commit [ci skip]
    
    * Replace logback-test.xm warn to info, add CIFLOW.adoc the 4th step, dependabot name is consequence now
    
    * License profile exclude .json files, generate headers and license files
    
    * Fix tabs and line ends
    
    * Add empty line in eof
    
    * Add the missing newlines in not binary files
    
    * Remove new line in maven.config
    
    * Remove new line from cfg
    
    * Add back newlines
    
    * Add new line to yml files
    
    * JNG-4625 Add the yml files the remove toolchains xml and setup maven toolchains xml steps
    
    * JNG-4625 Remove setup maven toolchains.xml action
    
    * JNG-4625 Remove id from setup-java action

--- model/src/main/java/hu/blackbelt/judo/meta/rdbms/runtime/RdbmsUtils.java
+++ model/src/main/java/hu/blackbelt/judo/meta/rdbms/runtime/RdbmsUtils.java
@@ -9,13 +9,13 @@ package hu.blackbelt.judo.meta.rdbms.runtime;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -461,7 +461,7 @@ public class RdbmsUtils {
      * @return model elements
      */
     @SuppressWarnings("unchecked")
-	<T> Stream<T> all() {
+    <T> Stream<T> all() {
         return asStream((Iterator<T>) resourceSet.getAllContents(), false);
     }
 
@@ -473,7 +473,7 @@ public class RdbmsUtils {
      * @return all elements with clazz type
      */
     @SuppressWarnings("unchecked")
-	public <T> Stream<T> all(final Class<T> clazz) {
+    public <T> Stream<T> all(final Class<T> clazz) {
         return all().filter(e -> clazz.isAssignableFrom(e.getClass())).map(e -> (T) e);
     }
 
